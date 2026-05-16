92f7dda1dbeaa72384ee385c1e44bf17b9dc69ae
Dhyan011 <Dhyan011@users.noreply.github.com>
2026-05-16 16:48:30 +0530

    linearsearch question

--- dsa/Linear Search/code/src/com/dhyan/FindMin.java
+++ dsa/Linear Search/code/src/com/dhyan/FindMin.java
@@ -4,7 +4,7 @@ import static java.util.Collections.min;
 
 public class FindMin {
     public static void main ( String[] args ){
-        int [] arr = {18 , 12 , -7 , 3 , 14 , 28  } ;
+        int [] arr = {18 , 12 , 7 , 3 , 14 , 28  } ;
         System.out.println(min(arr));
     }
     //assume arr.length ! = 0
